1eb38e0c7acd4d45f4e0cbc03ad2c9eb35450153
qilei <qilei2010@gmail.com>
2013-04-27 10:38:48 +0800

    app main

---
+++ src/main/java/app/AppMain.java
@@ -0,0 +1,15 @@
+package app;
+
+public class AppMain {
+
+	/**
+	 * Description: 
+	 * @Version1.0 2013-4-27 上午10:38:33 by 您的姓名（您的邮箱）创建
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
